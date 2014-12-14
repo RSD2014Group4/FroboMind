0c237bfbf7ebc61a821e4df1149c7dd07ccbf1c6
Martin Skriver <MaSkr09@student.sdu.dk>
2014-12-14 18:25:48 +0100

    Added msg to start/stop and deadman. Tipper action included

--- fmHMI/rqt_man/src/rqt_man/man_widget.py
+++ fmHMI/rqt_man/src/rqt_man/man_widget.py
@@ -2,11 +2,17 @@
 
 import os
 import rospkg
+import rospy
+import actionlib
 
 from python_qt_binding import loadUi
 from python_qt_binding.QtCore import Qt, QUrl
 from python_qt_binding.QtGui import QCompleter, QIcon, QWidget, QTableWidgetItem
 from python_qt_binding.QtWebKit import QWebPage, QWebView
+from TurboUlla.msg import oee_data
+#from rqt_man.msg import gui_command
+from lift_tipper.msg import tipperAction, tipperGoal
+
 
 class ManWidget(QWidget):
     def __init__(self):
@@ -28,6 +34,23 @@ class ManWidget(QWidget):
 	self.pushButtonDeadman.setStyleSheet("color: black; background-color: red")
 	self.deadmanActive = 'false'
 
+	self.labelName1.setText("Total Logging time: ")
+	self.labelName2.setText("Production percentage: ")
+	self.labelName3.setText("Manual percentage: ")
+
+	rospy.Subscriber("oee_data", oee_data, self.updateOEE)
+
+	self.tipperClient =  actionlib.SimpleActionClient('tipper_action',  tipperAction)
+
+	# Initialize publisher
+	self.pub = rospy.Publisher('gui_command', gui_command, queue_size=1)
+
+    def updateOEE(self, data):
+	self.labelValue1.setText(data.logfileTime)
+	self.labelValue2.setText(data.prodPercentageFromLogfileStart + ' [%]')
+	self.labelValue3.setText(data.manualPercentActiveRobot + ' [%]')
+	self.labelValue4.setText(data.shutdownPercentage)
+
     def _handle_deadman_clicked(self):
 	if self.deadmanActive == 'false':
 		self.deadmanActive = 'true'
@@ -43,14 +66,28 @@ class ManWidget(QWidget):
 	self.pushButtonAuto.setStyleSheet("background-color: green")
 	self.manualMode = 'false'
 
+	self.msg = gui_command()
+	self.msg.command = self.msg.COMMAND_AUTO
+	self.pub.publish(self.msg)
+
     def _handle_man_clicked(self):
         self.pushButtonAuto.setStyleSheet("background-color: light grey")
 	self.pushButtonMan.setStyleSheet("background-color: green")
+
+	self.msg = gui_command()
+	self.msg.command = self.msg.COMMAND_MAN	
+	self.pub.publish(self.msg)
 	self.manualMode = 'true'
 
-    def _handle_tp_R_S_clicked(self):
-        pass
 
+    def _handle_tp_R_S_clicked(self):
+	if self.manualMode == 'true':
+		rospy.loginfo("Tipping")
+        	goal = tipperGoal()
+		goal.lift = True;
+	        self.tipperClient.send_goal(goal)
+        	self.tipperClient.wait_for_result(rospy.Duration.from_sec(SERVER_WAIT_TIME))
+        
     def save_settings(self, settings):
         pass
 
